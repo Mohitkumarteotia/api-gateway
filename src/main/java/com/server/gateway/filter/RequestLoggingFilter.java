08588b75810c9021db8961aca6aad5cfbe4c15e6
MohitKumarteotia <teotiamohit011@gmail.com>
2026-08-29 20:40:46 +0530

    OK

--- src/main/java/com/server/gateway/filter/RequestLoggingFilter.java
+++ src/main/java/com/server/gateway/filter/RequestLoggingFilter.java
@@ -13,23 +13,16 @@ import reactor.core.publisher.Mono;
 public class RequestLoggingFilter implements GlobalFilter, Ordered {
 
     @Override
-    public Mono<Void> filter(ServerWebExchange exchange,
-                             GatewayFilterChain chain) {
-
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         long startTime = System.currentTimeMillis();
         String method = exchange.getRequest().getMethod().name();
         String path = exchange.getRequest().getURI().getPath();
         log.info("Incoming Request => Method: {}, Path: {}", method, path);
-        return chain.filter(exchange)
-                .then(Mono.fromRunnable(() -> {
-                    long executionTime =
-                            System.currentTimeMillis() - startTime;
-                    int status =
-                            exchange.getResponse()
-                                    .getStatusCode()
-                                    .value();
-                    log.info("Outgoing Response => Status: {}, Time: {} ms", status, executionTime);
-                }));
+        return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+            long executionTime = System.currentTimeMillis() - startTime;
+            int status = exchange.getResponse().getStatusCode().value();
+            log.info("Outgoing Response => Status: {}, Time: {} ms", status, executionTime);
+        }));
     }
 
     @Override
